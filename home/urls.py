5d40af9a3d130062cb49f065db5ba12d5fcc5344
esmaeil <eastudio1990@gmail.com>
2023-07-19 14:33:10 +0330

    first commit

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'home'
+
+urlpatterns=[
+   path('', views.Home.as_view(), name='home'),
+]
+
+ 
---
+++ home/views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class Home(APIView):
+    def get(self,request):
+        return Response({'name':'Esmaeil'})
