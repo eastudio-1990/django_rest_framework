6e273947936ab5f6f507ff316ef9002f73d7e599
esmaeil <eastudio1990@gmail.com>
2023-07-19 22:38:53 +0330

    serialize updated

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
 
 def clean_admin(value):
      if 'admin' in value:
          raise serializers.ValidationError(f'`{value}` CAN NOT CONTAIN `admin`')
 
 class UserSerializer(serializers.Serializer):
-    username = serializers.CharField(required = True,validators=[clean_admin])
-    email = serializers.EmailField(required = True,validators=[clean_admin])
-    password = serializers.CharField(required = True, write_only = True)
-    passwordConfirm = serializers.CharField(required = True, write_only = True)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
 
-    def validate(self,data):
-        if data['password'] != data['passwordConfirm']:
-            raise serializers.ValidationError('password ARE NOT SAME')
-        return data
+
+
+    # username = serializers.CharField(required = True,validators=[clean_admin])
+    # email = serializers.EmailField(required = True,validators=[clean_admin])
+    # password = serializers.CharField(required = True, write_only = True)
+    # passwordConfirm = serializers.CharField(required = True, write_only = True)
+
+    
+
+
+    # def validate(self,data):
+    #     if data['password'] != data['passwordConfirm']:
+    #         raise serializers.ValidationError('password ARE NOT SAME')
+    #     return data
     
 
     
