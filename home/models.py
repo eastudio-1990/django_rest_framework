b7f72dc4013a0bff7c92d9b0f3a6b8d9d21ae493
esmaeil <eastudio1990@gmail.com>
2023-07-19 19:03:45 +0330

    serializer added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.PositiveSmallIntegerField()
+    email =models.EmailField()
+
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Person
+from .serializers import PersonSerializer
+
 
 class Home(APIView):
     def get(self,request):
-        return Response({'name':'Esmaeil'})
-
+        person = Person.objects.all()    
+        serialized_person_data = PersonSerializer(instance= person,many= True)                   
+        return Response(data = serialized_person_data.data)
 
