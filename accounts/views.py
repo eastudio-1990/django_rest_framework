c82c08f9d801f1115d8027c1f20a8675ab32ccd2
esmaeil <eastudio1990@gmail.com>
2023-07-20 13:14:57 +0330

    status coeds added

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,14 @@
 from rest_framework.views import APIView 
 from rest_framework.response import Response
 from .serializers import UserRegisterSerializer
+from rest_framework import status
 
 class UserRegister(APIView):
     def post(self, request):
         serialized_user_data = UserRegisterSerializer(data=request.POST)
         if serialized_user_data.is_valid():
            serialized_user_data.create(serialized_user_data.validated_data)
-           return Response(serialized_user_data.data)               
-        return Response(serialized_user_data.errors)
+           return Response(serialized_user_data.data,status=status.HTTP_200_OK)               
+        return Response(serialized_user_data.errors,status=status.HTTP_400_BAD_REQUEST)
 
             
--- home/views.py
+++ home/views.py
@@ -2,11 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Person
 from .serializers import PersonSerializer
-
+from rest_framework import status
 
 class Home(APIView):
     def get(self,request):
         person = Person.objects.all()    
         serialized_person_data = PersonSerializer(instance= person,many= True)                   
-        return Response(data = serialized_person_data.data)
+        return Response(data = serialized_person_data.data,status=status.HTTP_200_OK)
 
