6e72b28cd456110fc97b69e2b5033fe7d924f0df
esmaeil <eastudio1990@gmail.com>
2023-07-25 16:24:57 +0330

    Throttling Added

--- A/settings.py
+++ A/settings.py
@@ -134,5 +134,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',        
-    ]
+    ],
+      'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/hour',
+        'user': '1000/day'
+    }
 }
